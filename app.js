531066e0b0a1738e662625a7435c4ccbf2230356
Himanshu Ragi <hirehimanshuragi@gmail.com>
2023-06-22 10:36:33 +0530

    fix: auth URL updated

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use(cors({
 }));
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Origin', ['https://droneshop-9de3e.web.app', 'http://localhost:3000']);
+  res.header('Access-Control-Allow-Origin', 'https://droneshop-9de3e.web.app');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
